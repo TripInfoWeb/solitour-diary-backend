3eb786490756f4c5aef1093f617a286f28fb5cd8
HyunJinNo <nhj9831@naver.com>
2024-08-13 19:56:35 +0900

    Feat: Diary 엔티티 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DiaryModule } from './modules/diary/diary.module';
+import { ConfigModule } from '@nestjs/config';
 
+// 모듈 데코레이터
 @Module({
-  imports: [DiaryModule],
+  imports: [
+    // ConfigModule 설정
+    // 1. 전역 모듈 설정 추가
+    // 2. 환경 변수 파일 경로 저장
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true, // 캐시하기, ConfigService의 get() 함수를 사용할 때 캐시에서 먼저 불러오게 되므로 성능상의 이점이 있음.
+    }),
+    DiaryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/entities/diary.entity.ts
@@ -0,0 +1,61 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Diary {
+  /**
+   * id는 primary key이며 자동으로 증가하는 값
+   */
+  @PrimaryGeneratedColumn({ name: 'id' })
+  id?: number;
+
+  /**
+   * 여행일기 제목
+   */
+  @Column('varchar', { name: 'title' })
+  title: string;
+
+  /**
+   * 시작 날짜
+   */
+  @Column('datetime', { name: 'state_date' })
+  startDate: Date;
+
+  /**
+   * 종료 날짜
+   */
+  @Column('datetime', { name: 'end_date' })
+  endDate: Date;
+
+  /**
+   * 주소명
+   *
+   * Ex. 서울 강남구
+   */
+  @Column('varchar', { name: 'address' })
+  address: string;
+
+  /**
+   * 생성 날짜
+   */
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  /**
+   * 수정 날짜
+   */
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  /**
+   * 삭제 날짜
+   */
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: Date | null;
+}
