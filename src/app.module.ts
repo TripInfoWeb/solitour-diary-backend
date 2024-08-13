89f190d858e2c6680a076fc399bff08cbc994e11
HyunJinNo <nhj9831@naver.com>
2024-08-13 19:34:22 +0900

    Feat: diary 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DiaryModule } from './modules/diary/diary.module';
 
 @Module({
-  imports: [],
+  imports: [DiaryModule],
   controllers: [AppController],
   providers: [AppService],
 })
