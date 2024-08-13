887adb0ffde26941aba2b2772e85567e6cfb5ec3
HyunJinNo <nhj9831@naver.com>
2024-08-13 22:04:53 +0900

    Refactor: 코드 리팩토링

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // ConfigService를 app.get()에 추가
+  const configService = app.get(ConfigService);
+
   // 전역 파이프에 validationPipe 객체 추가
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(configService.get('SERVER_PORT'));
 }
 bootstrap();
--- src/modules/image/image.controller.ts
+++ src/modules/image/image.controller.ts
@@ -4,17 +4,14 @@ import {
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
-import { ImageService } from './image.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { multerOptions } from 'src/multer.option';
 
 @Controller('api/image')
 export class ImageController {
-  constructor(private readonly imageService: ImageService) {}
-
   @Post('upload')
   @UseInterceptors(FileInterceptor('file', multerOptions)) // 파일 인터셉터
   uploadImage(@UploadedFile() file: Express.Multer.File) {
-    return `http://localhost:3000/uploads/images/diary/${file.filename}`;
+    return `http://localhost:${process.env.SERVER_PORT}/uploads/images/diary/${file.filename}`;
   }
 }
