4336728d92f2014ae85c95119a6d5394b9d98f7e
HyunJinNo <nhj9831@naver.com>
2024-08-13 20:55:27 +0900

    Feat: class-validator, class-transformer 패키지 설치

--- src/modules/diary/diary.module.ts
+++ src/modules/diary/diary.module.ts
@@ -3,9 +3,11 @@ import { DiaryController } from './diary.controller';
 import { DiaryService } from './diary.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Diary } from 'src/entities/diary.entity';
+import { User } from 'src/entities/user.entity';
+import { DiaryDay } from 'src/entities/diary-day.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Diary])],
+  imports: [TypeOrmModule.forFeature([User, Diary, DiaryDay])],
   controllers: [DiaryController],
   providers: [DiaryService],
 })
