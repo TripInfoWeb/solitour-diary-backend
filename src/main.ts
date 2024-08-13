cdd28823068ebb7c73ab1365b7529dc7fa817e87
HyunJinNo <nhj9831@naver.com>
2024-08-13 21:15:08 +0900

    Feat: 전역 파이프 사용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 전역 파이프에 validationPipe 객체 추가
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
