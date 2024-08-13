182bd00806d0f8e2eba4986f81aee90860e12756
HyunJinNo <nhj9831@naver.com>
2024-08-13 22:50:58 +0900

    Feat: 일기 작성 API 구현

--- src/modules/diary/diary.controller.ts
+++ src/modules/diary/diary.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { DiaryService } from './diary.service';
+import { CreateDiaryDto } from './diary.dto';
 
 @Controller('api/diary')
 export class DiaryController {
   constructor(private readonly diaryService: DiaryService) {}
+
+  /**
+   * 일기 작성
+   * @param diaryDto 일기 데이터
+   * @returns 등록된 일기 id 값
+   */
+  @Post()
+  async createDiary(@Body() diaryDto: CreateDiaryDto) {
+    return await this.diaryService.createDiary(diaryDto);
+  }
 }
--- src/modules/diary/diary.service.ts
+++ src/modules/diary/diary.service.ts
@@ -1,4 +1,79 @@
 import { Injectable } from '@nestjs/common';
+import { CreateDiaryDto } from './diary.dto';
+import { Diary } from 'src/entities/diary.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DiaryDay } from 'src/entities/diary-day.entity';
 
-@Injectable()
-export class DiaryService {}
+@Injectable() // 의존성 주입을 위한 데코레이터
+export class DiaryService {
+  // Repository 주입
+  constructor(
+    @InjectRepository(Diary) private diaryRepository: Repository<Diary>,
+    @InjectRepository(DiaryDay)
+    private diaryDayRepository: Repository<DiaryDay>,
+  ) {}
+
+  /**
+   * 일기 작성
+   * @param diaryDto 일기 데이터
+   */
+  async createDiary(diaryDto: CreateDiaryDto) {
+    // 일기 정보 등록
+    const diary = new Diary();
+    diary.userId = diaryDto.userId;
+    diary.title = diaryDto.title;
+    diary.startDate = diaryDto.startDate;
+    diary.endDate = diaryDto.endDate;
+    diary.address = diaryDto.address;
+    const newDiary = await this.diaryRepository.save(diary);
+
+    // 일기 day 정보 배열 생성
+    const diaryDays = diaryDto.diaryDays.map((value, index) => {
+      const diaryDay = new DiaryDay();
+      diaryDay.diaryId = newDiary.id;
+      diaryDay.day = index + 1;
+      diaryDay.moodLevel = value.moodLevel;
+      diaryDay.content = value.content;
+      return diaryDay;
+    });
+
+    // bulk insert
+    await this.diaryDayRepository
+      .createQueryBuilder()
+      .insert()
+      .into(DiaryDay)
+      .values(diaryDays)
+      .execute();
+
+    return newDiary.id;
+  }
+
+  /**
+   * 일기 상세 조회
+   */
+  async getDiary(id: number) {
+    return await this.diaryRepository.findOneOrFail({ where: { id } });
+  }
+
+  /**
+   * 일기 목록 조회
+   */
+  getDiaryList() {
+    // TODO
+  }
+
+  /**
+   * 일기 수정
+   */
+  updateDiary() {
+    // TODO
+  }
+
+  /**
+   * 일기 삭제
+   */
+  deleteDiary() {
+    // TODO
+  }
+}
